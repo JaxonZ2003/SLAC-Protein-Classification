1331c929310beee2b625e68ad5d1ef43ee25c907
Shirleywww <shuhanwang@umail.ucsb.edu>
2025-02-21 15:33:18 -0800

    added data_split.py

--- data_split.py
+++ data_split.py
@@ -1,27 +1,32 @@
 import random
 import pandas as pd
+import numpy as np
 
 from dataset import ImageDataset
 
-def split_train_val(train_csv, test_csv):
+def split_train_val(train_csv, test_csv, seed = 1):
     """
     Splits a dataset into a smaller training set and a validation set.
     The validation set size = the test set size
     """
+
+    random.seed(seed)
+    np.random.seed(seed)
+
     train_df = pd.read_csv(train_csv)
     test_df = pd.read_csv(test_csv)
 
     test_size = len(test_df)
 
-    train_shuffled = train_df.sample(frac = 1).reset_index(drop=True)
+    # Shuffle data with a fixed random seed
+    train_shuffled = train_df.sample(frac=1, random_state=seed).reset_index(drop=True)
     val_df = train_shuffled.iloc[:test_size]
     new_train_df = train_shuffled.iloc[test_size:]
 
     new_train_df.to_csv("./data/new_train_info.csv", index=False)
     val_df.to_csv("./data/val_info.csv", index=False)
 
-
-    print(f"New training set size:{len(new_train_df)}")
+    print(f"New training set size: {len(new_train_df)}")
     print(f"Validation set size: {len(val_df)}")
     print("Validation Set Sample:")
     print(val_df.head())
@@ -29,4 +34,4 @@ def split_train_val(train_csv, test_csv):
     print(new_train_df.head())
 
 if __name__ == "__main__":
-    split_train_val("./data/train_info.csv", "./data/test_info.csv")
+    split_train_val("./data/train_info.csv", "./data/test_info.csv", seed=1)
