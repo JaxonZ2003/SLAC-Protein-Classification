7d1a8413686d8dab3ae900835403df78b177349b
Reese Karo <reesekaro@gmail.com>
2025-02-23 11:25:01 -0800

    Update to dataset.py file adding in Normalizing to the images for potential better performance

--- jobs.sh
+++ jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J slac_train_testing ## Job Name
+#SBATCH -J CNN_Baseline_10Epochs ## Job Name
 #SBATCH -N 1 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -7,10 +7,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH -o /scratch/slac/slurmlogs/outLog_%x_%j.txt ### Output Log File (Optional)
 #SBATCH -e /scratch/slac/slurmlogs/errLog_%x_%j.txt ### Error Log File (Optional but suggest to have it)
-#SBATCH -t 1:00:00 ### Job Execution Time, 1 hour to see if it is running.
+#SBATCH -t 10:00:00 ### Job Execution Time, 10 hour to see if it is running.
 
 LR=0.001
-srun -c 4 python ~/capstone-SLAC/Model_trainer.py --lr $LR 10
+srun -c 4 python ~/capstone-SLAC/Model_trainer.py --nepoch 10 --lr $LR
 
 
 #srun -c 4 /home/reesekaro/train_wrapper.sh $LR
