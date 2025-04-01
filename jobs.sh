b417c6e86781d777c342d982a3f817b64b775895
Shirleywww <shuhanwang@umail.ucsb.edu>
2025-04-01 15:46:28 -0700

    Updated jobs.sh with new changes

--- jobs.sh
+++ jobs.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH -J CNN_Baseline_30Epochs_0.01LR ## Job Name
+#SBATCH -J CNN_Baseline_30Epochs_0.01LR
 #SBATCH -N 1 
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --partition=gpu ## Request 1 GPU   
+#SBATCH --cpus-per-task=2
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH -o /scratch/slac/slurmlogs/outLog_%x_%j.txt ### Output Log File (Optional)
-#SBATCH -e /scratch/slac/slurmlogs/errLog_%x_%j.txt ### Error Log File (Optional but suggest to have it)
-#SBATCH -t 15:00:00 ### Job Execution Time
+#SBATCH -o ~/capstone-SLAC/logs/outLog_%x_%j.txt
+#SBATCH -e ~/capstone-SLAC/logs/errLog_%x_%j.txt
+#SBATCH -t 15:00:00
 
 LR=0.001
-srun -c 4 python ./__main__.py --num_workers 4 --nepoch 20 --lr $LR
+srun -c 2 python ./__main__.py --num_workers 4 --nepoch 20 --lr $LR
+
 
 
 #srun -c 4 /home/reesekaro/train_wrapper.sh $LR
