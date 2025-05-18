a589321abed0c00b2208752b1cdd7537e7574bf5
Rebecca Chang <rebeccachang@ucsb.edu>
2025-05-18 10:47:15 -0700

    added updated lrloop.py file

--- SLAC25/scripts/lrloop.py
+++ SLAC25/scripts/lrloop.py
@@ -1,12 +1,16 @@
 import os
+import sys
 
-for lr in [0.001, 0.01, 0.1]:
-    out = f"/scratch/slac/slurmlogs/%j_lr_{lr}.out"
-    err = f"/scratch/slac/slurmlogs/%j_lr_{lr}.err"
-    odir = f"/scratch/slac/rcmodels/lr_{lr}"
-    jobname = f"lr_{lr}"
-    script = "/home/rebeccachang/capstone-SLAC/__main__.py"
-    cmd = f'sbatch  -J {jobname} -N 1 --ntasks-per-node=1 -p gpu  -t 24:00:00  -o {out} -e {err} --gres=gpu:1 --wrap="python {script} --nepoch 100 --lr {lr} --batch_size=64 --verbose --maxImgs 10000 --outdir {odir}"'
+tag = sys.argv[1]
+
+queue="regular"
+for i, lr in enumerate([0.001, 0.01, 0.1]):
+    out = f"/pscratch/sd/r/rebecca/slurmlogs/{tag}_%j_lr_{lr}.out"
+    err = f"/pscratch/sd/r/rebecca/slurmlogs/{tag}_%j_lr_{lr}.err"
+    odir = f"/pscratch/sd/r/rebecca/rcmodels/{tag}_lr_{lr}"
+    jobname = f"{tag}.{i}"
+    script = "/global/homes/r/rebecca/capstone-SLAC/__main__.py"
+    cmd = f'sbatch  -J {jobname} -N 1 --ntasks-per-node=1 -A m4731_g -t 24:00:00  -o {out} -e {err} --gres=gpu:1 --wrap="python {script} --nepoch 100 --lr {lr} --batch_size=64 --verbose --maxImgs 10000 --nwork 0 --outdir {odir}" -q {queue} -C gpu'
     print(cmd)
 
     os.system(cmd)
