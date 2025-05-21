5a3fdfc4eec451c114dc00cf4759d17af06579c1
Shirleywww <shuhanwang@umail.ucsb.edu>
2025-05-20 17:16:11 -0700

    vae update

--- SLAC25/VAE_train.py
+++ SLAC25/VAE_train.py
@@ -14,11 +14,15 @@ LATENT_DIM = 4
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(DEVICE)
 
+from torch.utils.data import Subset
 dataset = ImageDataset("../data/train_info.csv", transform=None, config=None, recordTransform=False)
-print(dataset[0])
+dataset = Subset(dataset, list(range(10000)))
+print("loaded %d images"%len(dataset))
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 print(dataloader)
 
+nbatches=len(dataloader)
+print(f"Number of batches={nbatches}")
 vae = MyVAE(latent_dim=LATENT_DIM).to(DEVICE)
 print(vae)
 optimizer = optim.Adam(vae.parameters(), lr=1e-4)
@@ -31,7 +35,7 @@ for epoch in range(EPOCHS):
     vae.train()
     total_loss = 0
     for i, (imgs, _) in enumerate(dataloader):
-        print(epoch,i)
+        print(f"processing epoch {epoch+1}, batch {i+1}/{nbatches} (batchsize={len(imgs)})", flush=True, end="\r")
         imgs = imgs.to(DEVICE, dtype=torch.float32)
         recon, mu, logvar = vae(imgs)
         loss = vae.loss_function(recon, imgs, mu, logvar)
@@ -44,7 +48,7 @@ for epoch in range(EPOCHS):
         if i == 0:
             vutils.save_image(imgs[:4], f"{odir}/epoch{epoch+1}_input.png", nrow=2)
             vutils.save_image(recon[:4], f"{odir}/epoch{epoch+1}_recon.png", nrow=2)
-
+    print()
     print(f"Epoch {epoch+1}: Loss = {total_loss / len(dataloader):.6f}")
 
 torch.save(vae.state_dict(), f"{odir}/vae_model_{epoch+1}.pth")
