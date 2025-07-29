c68d1a660734235590254724a1f98713e6d773c9
Jaxon Zhang <yangzhang@ucsb.edu>
2025-07-28 21:51:36 -0700

    kornia for transform in GPU

--- SLAC25/dataset.py
+++ SLAC25/dataset.py
@@ -11,11 +11,11 @@ from torchvision.transforms import v2
 from PIL import Image
 from datetime import datetime
 
-from SLAC25.transform import TransformV1, preprocess
+from SLAC25.transform import TransformV1, Preprocess
 
 
 class ImageDataset(Dataset):
-  def __init__(self, csvfilePath, transform=preprocess, config=None, recordTransform=False):
+  def __init__(self, csvfilePath, transform=None):
     self.csvfilePath = csvfilePath
     self.dataframe = pd.read_csv(csvfilePath)
     self.transform = transform
@@ -23,8 +23,6 @@ class ImageDataset(Dataset):
     # self.datasetType = self._checkTrainTest()
     # self.config = self._loadConfig()
     # self._setupTransform(transform, config, recordTransform)
-
-    # self.datasize = self.dataframe.shape[0]
     self.numLabel = self.dataframe['label_id'].nunique()
     # self.labeldict = {idnum: self.dataframe.index[self.dataframe['label_id'] == idnum].to_list() for idnum in self.dataframe['label_id'].value_counts().index}
 
@@ -40,7 +38,6 @@ class ImageDataset(Dataset):
       idx = idx.tolist()
 
     img_path, label_row = self.getPathAndLabel(idx)
-
     img = Image.open(img_path)
 
     if self.transform:
@@ -52,16 +49,6 @@ class ImageDataset(Dataset):
   def getPathAndLabel(self, idx):
     row = self.dataframe.iloc[idx]
     return (row['image_path'], row['label_id'])
-  
-  # def getImagePath(self, idx):
-  #   row = self.dataframe.iloc[idx]
-  #   # img_label_text = row['label_text']
-  #   # img_path = os.path.join(self.root_dir, img_label_text)
-  #   return row['image_path']
-  
-  # def getLabelId(self, idx):
-  #   row = self.dataframe.iloc[idx]
-  #   return row['label_id']
   
   # def _checkTrainTest(self):
   #   if "test_info" in self.csvfilePath:
@@ -93,64 +80,64 @@ class ImageDataset(Dataset):
   #     self.transform = transform
 
   
-  def visualizeAndSave(self, idx, savedPath=None):
-    if savedPath is None:
-      package_root = os.path.dirname(os.path.abspath(__file__))
-      savedPath = os.path.join(package_root, "..", "img")
-      savedPath = os.path.abspath(savedPath)
+  # def visualizeAndSave(self, idx, savedPath=None):
+  #   if savedPath is None:
+  #     package_root = os.path.dirname(os.path.abspath(__file__))
+  #     savedPath = os.path.join(package_root, "..", "img")
+  #     savedPath = os.path.abspath(savedPath)
 
-    os.makedirs(savedPath, exist_ok=True) # make a dir if not exists
-    titleBefT_params = self.config["visualizeAndSave"]["title_before_transform"]
-    titleAftT_params = self.config["visualizeAndSave"]["title_after_transform"]
-    label_params = self.config["visualizeAndSave"]["label_text_params"]
-    label_params['s'] = label_params['s'].format(self.getLabelId(idx))
-    label_transformlogs = self.config["visualizeAndSave"]["label_transform_logs"]
+  #   os.makedirs(savedPath, exist_ok=True) # make a dir if not exists
+  #   titleBefT_params = self.config["visualizeAndSave"]["title_before_transform"]
+  #   titleAftT_params = self.config["visualizeAndSave"]["title_after_transform"]
+  #   label_params = self.config["visualizeAndSave"]["label_text_params"]
+  #   label_params['s'] = label_params['s'].format(self.getLabelId(idx))
+  #   label_transformlogs = self.config["visualizeAndSave"]["label_transform_logs"]
 
     
-    timeNow = datetime.now(pytz.timezone("America/Los_Angeles")).strftime("%Y%m%d%H%M%S")
-    filename = os.path.join(savedPath, f"{self.datasetType}_{timeNow}_{idx}.png")
-    filename = os.path.abspath(filename)
+  #   timeNow = datetime.now(pytz.timezone("America/Los_Angeles")).strftime("%Y%m%d%H%M%S")
+  #   filename = os.path.join(savedPath, f"{self.datasetType}_{timeNow}_{idx}.png")
+  #   filename = os.path.abspath(filename)
 
-    ### Image Before Transform ###
-    imgBefT = Image.open(self.getImagePath(idx))
+  #   ### Image Before Transform ###
+  #   imgBefT = Image.open(self.getImagePath(idx))
     
-    ### Image After Transform ###
-    res = self.__getitem__(idx)
-    imgAftT = res[0].clone().detach().cpu()
-    label_transformlogs['s'] = label_transformlogs['s'].format(self.transform._getLog(idx))
-    imgAftTnp = imgAftT.numpy().transpose(1, 2, 0)
+  #   ### Image After Transform ###
+  #   res = self.__getitem__(idx)
+  #   imgAftT = res[0].clone().detach().cpu()
+  #   label_transformlogs['s'] = label_transformlogs['s'].format(self.transform._getLog(idx))
+  #   imgAftTnp = imgAftT.numpy().transpose(1, 2, 0)
 
 
-    ### Plot ###
-    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-    ax[0].imshow(imgBefT)
-    ax[0].axis('off')
-    ax[1].imshow(imgAftTnp)
-    ax[1].axis('off')
-    # fig.text(**titleBefT_params)
-    # fig.text(**titleAftT_params)
-    # fig.text(**label_params)
-    fig.text(**label_transformlogs)
-    plt.tight_layout()
-    plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
-    plt.close(fig)
+  #   ### Plot ###
+  #   fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+  #   ax[0].imshow(imgBefT)
+  #   ax[0].axis('off')
+  #   ax[1].imshow(imgAftTnp)
+  #   ax[1].axis('off')
+  #   # fig.text(**titleBefT_params)
+  #   # fig.text(**titleAftT_params)
+  #   # fig.text(**label_params)
+  #   fig.text(**label_transformlogs)
+  #   plt.tight_layout()
+  #   plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+  #   plt.close(fig)
 
   
-  def summary(self):
-    '''
-    prints a summary of the dataset
-    '''
-    print(f"\n{'='*40}")
-    print(f"{' '*12}Dataset Summary")
-    print(f"{'-'*40}")
-    print(f"File Path:    {self.csvfilePath}")
-    print(f"Last Updated: {self.dataLastModified}")
-    print(f"{'-'*40}")
-    print(f"Sample Sizes: {self.datasize}")
-    print(f"Label Types: {self.numLabel}")
-    for l in range(self.numLabel):
-      print(f"Label {l}: {len(self.labeldict[l])} | {len(self.labeldict[l]) / self.datasize * 100:.2f}%")
-    print(f"{'='*40}")
+  # def summary(self):
+  #   '''
+  #   prints a summary of the dataset
+  #   '''
+  #   print(f"\n{'='*40}")
+  #   print(f"{' '*12}Dataset Summary")
+  #   print(f"{'-'*40}")
+  #   print(f"File Path:    {self.csvfilePath}")
+  #   print(f"Last Updated: {self.dataLastModified}")
+  #   print(f"{'-'*40}")
+  #   print(f"Sample Sizes: {self.datasize}")
+  #   print(f"Label Types: {self.numLabel}")
+  #   for l in range(self.numLabel):
+  #     print(f"Label {l}: {len(self.labeldict[l])} | {len(self.labeldict[l]) / self.datasize * 100:.2f}%")
+  #   print(f"{'='*40}")
  
 
 
--- SLAC25/transform.py
+++ SLAC25/transform.py
@@ -2,151 +2,177 @@ import random
 import torch
 
 from torchvision.transforms import v2
+from kornia.augmentation import RandomCrop, RandomAffine
+import torch.nn as nn
 # from PIL import Image
 
-
-preprocess = v2.Compose([
-  v2.Resize((512, 512)),
-  v2.PILToTensor()
-])
-
-
-tfConfig = {
-    "rotation": {
-        "on": True,
-        "params": {
-            "degrees": (-45, 45)
-        }
-    },
-    "horizontal_flip": {
-        "on": True
-    },
-    "vertical_flip": {
-        "on": True
-    },
-    "gaussian_blur": {
-        "on": True,
-        "params": {
-            "kernel_size": 5,
-            "sigma": (0.1, 2.0)
-        }
-    },
-    "sharpening": {
-        "on": True,
-        "params": {
-            "factor": 2.0
-        }
-    }
-}
-
-class TransformV1():
-  def __init__(self, config=None, recordTransform=False):
-    self.tf_log = None
-    self.tf_config = None
-
-    self._setTfConfig(config)
-    self._setTfLog(recordTransform) # use to record transformation info
-    self.preprocessing = v2.Compose([
-      v2.Resize((512, 512), interpolation=v2.InterpolationMode.BILINEAR, antialias=True),
-      v2.PILToTensor(),
-      v2.ConvertImageDtype(torch.float32)
+class Preprocess:
+  def __init__(self, size=(512, 512)):
+    self.transform = v2.Compose([
+      v2.Resize(size),
+      v2.PILToTensor()
     ])
-
-  def _setTfLog(self, recordTf):
-    if not recordTf:
-      return
-
-    else:
-      self.tf_log = {}
   
-  def _setTfConfig(self, config):
-    if not config:
-      self.tf_config = tfConfig
-    
-    else:
-      self.tf_config = config
-  
-  def _checkConfig(self):
-    if not self.tf_config:
-      return False
-    
-    else:
-      return True
-  
-  def _recordTf(self, idx, action):
-    if self.tf_config is None:
-      return
-    
-    if not self.tf_config.get(idx):
-       self.tf_config[idx] = []
-    
-    self.tf_config[idx].append(str(action))
+  def __call__(self, img):
+    return self.transform(img)
 
-  def _emptyRecord(self, idx):
-    if self.tf_log is None:
-      return
-    
-    if not self.tf_config.get(idx):
-      return
-    
-    else:
-      self.tf_config[idx].clear()
-
-  def _getLog(self, idx):
-    if self.tf_log is None:
-      return
-    
-    if not self.tf_config.get(idx):
-      return
-    
-    res = '\n'.join(map(str, self.tf_config[idx]))
-    
-    return res
-
-  def _random_gaussian_blur(self, img, idx):
-    """Gaussian blur with 50% probability"""
-    if self._checkConfig() and self.tf_config["gaussian_blur"]["on"]:
-      params = self.tf_config["gaussian_blur"]["params"]
-      if random.random() < 0.5:
-        self._recordTf(idx, f"Gaussian Blur: {str(params)}")
-        return v2.GaussianBlur(**params)(img)
-
-    return img
-
-  def _random_rotation(self, img, idx):
-    """Random rotation between -45 to 45 degrees with a 50% probability"""
-    if self._checkConfig() and self.tf_config["rotation"]["on"]:
-      params = self.tf_config["rotation"]["params"]
-      if random.random() < 0.5:
-        angle = random.uniform(*params["degrees"])
-        self._recordTf(idx, f"Rotating by {angle:.2f} degrees")
-        return v2.RandomRotation(degrees=(angle, angle))(img)
-    
-    return img
-
-  def _random_horizontal_flip(self, img, idx):
-    """Horizontal flip with 50% probability"""
-    if self._checkConfig() and self.tf_config["horizontal_flip"]["on"]:
-      if random.random() < 0.5:
-        self._recordTf(idx, "Applying Horizontal Flip")
-        return v2.RandomHorizontalFlip()(img)
+class DataAugmentation(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.transform = nn.Sequential(
+      RandomAffine(degrees=179, 
+                   scale=(1.0, 1.2), 
+                   shear=20,
+                   translate=(0.1, 0.1),
+                   p=0.5),
+      CenterCrop(size=(300,300)),
       
-    return img
-
-  def _random_vertical_flip(self, img, idx):
-    """Vertical flip with 50% probability"""
-    if self._checkConfig() and self.tf_config["horizontal_flip"]["on"]:
-      if random.random() < 0.5:
-        self._recordTf(idx, "Applying Vertical Flip")
-        return v2.RandomVerticalFlip()(img)
-      
-    return img
+    )
   
-  def _random_sharpening(self, img, idx):
-    """Sharpening the imag with 50% probability"""
-    if self._checkConfig() and self.tf_config["sharpening"]["on"]:
-      if random.random() < 0.5:
-        self._recordTf(idx, "Applying Sharpening")
-        return v2.RandomAdjustSharpness()(img)
+  @torch.no_grad()
+  def forward(self, img):
+    x_out = self.transform(img)
+    return x_out
+
+
+
+
+# tfConfig = {
+#     "rotation": {
+#         "on": True,
+#         "params": {
+#             "degrees": (-45, 45)
+#         }
+#     },
+#     "horizontal_flip": {
+#         "on": True
+#     },
+#     "vertical_flip": {
+#         "on": True
+#     },
+#     "gaussian_blur": {
+#         "on": True,
+#         "params": {
+#             "kernel_size": 5,
+#             "sigma": (0.1, 2.0)
+#         }
+#     },
+#     "sharpening": {
+#         "on": True,
+#         "params": {
+#             "factor": 2.0
+#         }
+#     }
+# }
+
+# class TransformV1():
+#   def __init__(self, config=None, recordTransform=False):
+#     self.tf_log = None
+#     self.tf_config = None
+
+#     self._setTfConfig(config)
+#     self._setTfLog(recordTransform) # use to record transformation info
+#     self.preprocessing = v2.Compose([
+#       v2.Resize((512, 512), interpolation=v2.InterpolationMode.BILINEAR, antialias=True),
+#       v2.PILToTensor(),
+#       v2.ConvertImageDtype(torch.float32)
+#     ])
+
+#   def _setTfLog(self, recordTf):
+#     if not recordTf:
+#       return
+
+#     else:
+#       self.tf_log = {}
+  
+#   def _setTfConfig(self, config):
+#     if not config:
+#       self.tf_config = tfConfig
     
-    return img
+#     else:
+#       self.tf_config = config
+  
+#   def _checkConfig(self):
+#     if not self.tf_config:
+#       return False
+    
+#     else:
+#       return True
+  
+#   def _recordTf(self, idx, action):
+#     if self.tf_config is None:
+#       return
+    
+#     if not self.tf_config.get(idx):
+#        self.tf_config[idx] = []
+    
+#     self.tf_config[idx].append(str(action))
+
+#   def _emptyRecord(self, idx):
+#     if self.tf_log is None:
+#       return
+    
+#     if not self.tf_config.get(idx):
+#       return
+    
+#     else:
+#       self.tf_config[idx].clear()
+
+#   def _getLog(self, idx):
+#     if self.tf_log is None:
+#       return
+    
+#     if not self.tf_config.get(idx):
+#       return
+    
+#     res = '\n'.join(map(str, self.tf_config[idx]))
+    
+#     return res
+
+#   def _random_gaussian_blur(self, img, idx):
+#     """Gaussian blur with 50% probability"""
+#     if self._checkConfig() and self.tf_config["gaussian_blur"]["on"]:
+#       params = self.tf_config["gaussian_blur"]["params"]
+#       if random.random() < 0.5:
+#         self._recordTf(idx, f"Gaussian Blur: {str(params)}")
+#         return v2.GaussianBlur(**params)(img)
+
+#     return img
+
+#   def _random_rotation(self, img, idx):
+#     """Random rotation between -45 to 45 degrees with a 50% probability"""
+#     if self._checkConfig() and self.tf_config["rotation"]["on"]:
+#       params = self.tf_config["rotation"]["params"]
+#       if random.random() < 0.5:
+#         angle = random.uniform(*params["degrees"])
+#         self._recordTf(idx, f"Rotating by {angle:.2f} degrees")
+#         return v2.RandomRotation(degrees=(angle, angle))(img)
+    
+#     return img
+
+#   def _random_horizontal_flip(self, img, idx):
+#     """Horizontal flip with 50% probability"""
+#     if self._checkConfig() and self.tf_config["horizontal_flip"]["on"]:
+#       if random.random() < 0.5:
+#         self._recordTf(idx, "Applying Horizontal Flip")
+#         return v2.RandomHorizontalFlip()(img)
+      
+#     return img
+
+#   def _random_vertical_flip(self, img, idx):
+#     """Vertical flip with 50% probability"""
+#     if self._checkConfig() and self.tf_config["horizontal_flip"]["on"]:
+#       if random.random() < 0.5:
+#         self._recordTf(idx, "Applying Vertical Flip")
+#         return v2.RandomVerticalFlip()(img)
+      
+#     return img
+  
+#   def _random_sharpening(self, img, idx):
+#     """Sharpening the imag with 50% probability"""
+#     if self._checkConfig() and self.tf_config["sharpening"]["on"]:
+#       if random.random() < 0.5:
+#         self._recordTf(idx, "Applying Sharpening")
+#         return v2.RandomAdjustSharpness()(img)
+    
+#     return img
